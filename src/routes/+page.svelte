b028602fbecc83a442a5071e7a7eec316d798a5b
Rongrong Xu <165723239+Cassy081@users.noreply.github.com>
2024-10-23 12:25:14 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <h1>Rongrong Xu</h1>
 <p>I'm a data science major student passionate about using data to solve real-world problems.</p>
 <!-- svelte-ignore a11y_img_redundant_alt -->
-<img src="/images/WechatIMG7.jpg" alt="A photo of me, a data science major student" width="300" height="300">
+<img src="./images/WechatIMG7.jpg" alt="A photo of me, a data science major student" width="300" height="300">
 
 <h2>Latest Projects</h2>
 <div class="projects highlights">
