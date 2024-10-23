b9cdb7e23d58a94a3f79c98bae5d4d44a3147f9a
Cassy Xu <ROX002@UCSD.EDU>
2024-10-22 18:35:35 -0700

    1
    1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,4 @@
 <h1>Rongrong Xu</h1>
 <p>I'm a data scientist major student passionate about using data to solve real-world problems.</p>
 <!-- svelte-ignore a11y_img_redundant_alt -->
-<img src="/images/WechatIMG7.jpg" alt="A photo of me, a data scientist major student" width="300" height="300">
+<img src="/static/images/WechatIMG7.jpg" alt="A photo of me, a data scientist major student" width="300" height="300">
